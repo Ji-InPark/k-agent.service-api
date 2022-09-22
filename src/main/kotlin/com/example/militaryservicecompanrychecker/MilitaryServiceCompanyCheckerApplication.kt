2366c640f61fefa2b4ac576d27c138b11a84df12
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-22 21:01:54 +0900

    Rename application and modify http method

--- src/main/kotlin/com/example/militaryservicecompanrychecker/MilitaryServiceCompanyCheckerApplication.kt
+++ src/main/kotlin/com/example/militaryservicecompanrychecker/MilitaryServiceCompanyCheckerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MilitaryServiceCompanryCheckerApplication
+class MilitaryServiceCompanyCheckerApplication
 
 fun main(args: Array<String>) {
-    runApplication<MilitaryServiceCompanryCheckerApplication>(*args)
+    runApplication<MilitaryServiceCompanyCheckerApplication>(*args)
 }
