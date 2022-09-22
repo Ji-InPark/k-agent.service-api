b6dc9fd8dcbe3f493899503898f1350976cca70f
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2022-09-22 21:02:37 +0900

    0.0.1 (#1)
    * Set base url
    
    * Fix typo and Update gradle
    
    * Rename application and modify http method

--- src/main/kotlin/com/example/militaryservicecompanrychecker/company/controller/CompanyController.kt
+++ src/main/kotlin/com/example/militaryservicecompanrychecker/company/controller/CompanyController.kt
@@ -1,11 +1,10 @@
 package com.example.militaryservicecompanrychecker.company.controller
 
 import com.example.militaryservicecompanrychecker.company.CompanyService
-import com.example.militaryservicecompanrychecker.company.controller.dto.CompanyRequest
 import com.example.militaryservicecompanrychecker.company.controller.dto.CompanyResponse
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,8 +12,8 @@ class CompanyController(
     @Autowired
     private val companyService: CompanyService
 ) {
-    @PostMapping("/companies")
-    fun searchCompany(@RequestBody request: CompanyRequest): CompanyResponse {
-        return CompanyResponse(companyService.searchCompany(request.companyName))
+    @GetMapping("/companies/{companyName}")
+    fun searchCompany(@PathVariable("companyName") companyName: String): CompanyResponse {
+        return CompanyResponse(companyService.searchCompany(companyName))
     }
 }
