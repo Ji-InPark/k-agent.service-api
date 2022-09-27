f1d852ff5d75e57f24249f5db63b0c54384dc9ed
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-27 22:01:02 +0900

    Update cross origin domain

--- src/main/kotlin/com/example/militaryservicecompanychecker/company/controller/CompanyController.kt
+++ src/main/kotlin/com/example/militaryservicecompanychecker/company/controller/CompanyController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin(origins = ["https://anydomaintotest.ml/"])
+@CrossOrigin(origins = ["https://k-agent.services/"])
 class CompanyController(
     @Autowired
     private val companyService: CompanyService
