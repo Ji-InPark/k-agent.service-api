e2fa1c19c903699a7c18a4bb5885eaf630ad79d4
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-27 20:06:59 +0900

    Squashed commit of the following:
    commit 7c09fcb06c6c6193f9b784a5d0305bc621adabad
    Author: Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
    Date:   Tue Sep 27 20:06:45 2022 +0900
    
        Add cross origin annotation

--- src/main/kotlin/com/example/militaryservicecompanychecker/company/controller/CompanyController.kt
+++ src/main/kotlin/com/example/militaryservicecompanychecker/company/controller/CompanyController.kt
@@ -3,11 +3,13 @@ package com.example.militaryservicecompanychecker.company.controller
 import com.example.militaryservicecompanychecker.company.CompanyService
 import com.example.militaryservicecompanychecker.company.controller.dto.CompanyResponse
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["https://anydomaintotest.ml/"])
 class CompanyController(
     @Autowired
     private val companyService: CompanyService
