8b0b43db49659efcb68dfe93b990ac8eb4ebb145
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-10-06 00:40:23 +0900

    Refactor and add some files

---
+++ src/main/kotlin/com/example/militaryservicecompanychecker/company/repository/CompanyRepository.kt
@@ -0,0 +1,10 @@
+package com.example.militaryservicecompanychecker.company.repository
+
+import com.example.militaryservicecompanychecker.company.entity.Company
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CompanyRepository : JpaRepository<Company, String> {
+    fun findCompaniesByCompanyNameContaining(companyName: String): List<Company>
+}
