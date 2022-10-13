758213db450b533a1eff9c073278a7163f58dcd1
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-10-13 20:17:41 +0900

    rename field

--- src/main/kotlin/com/example/militaryservicecompanychecker/company/controller/dto/CompanyResponse.kt
+++ src/main/kotlin/com/example/militaryservicecompanychecker/company/controller/dto/CompanyResponse.kt
@@ -2,6 +2,6 @@ package com.example.militaryservicecompanychecker.company.controller.dto
 
 import com.example.militaryservicecompanychecker.company.entity.Company
 
-data class CompanyResponse(val Companies: List<Company>) {
-    val companyCount get() = Companies.count()
+data class CompanyResponse(val companies: List<Company>) {
+    val companyCount get() = companies.count()
 }
