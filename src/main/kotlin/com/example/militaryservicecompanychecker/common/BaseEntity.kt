75296dc46fc733bae47a6e61f2a491af94707f02
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-07-05 00:01:11 +0900

    Add BaseEntity for audit

---
+++ src/main/kotlin/com/example/militaryservicecompanychecker/common/BaseEntity.kt
@@ -0,0 +1,21 @@
+package com.example.militaryservicecompanychecker.common
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @CreatedDate
+    var createdAt: LocalDateTime? = LocalDateTime.MIN
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = LocalDateTime.MIN
+}
--- src/main/kotlin/com/example/militaryservicecompanychecker/company/entity/Company.kt
+++ src/main/kotlin/com/example/militaryservicecompanychecker/company/entity/Company.kt
@@ -1,5 +1,6 @@
 package com.example.militaryservicecompanychecker.company.entity
 
+import com.example.militaryservicecompanychecker.common.BaseEntity
 import com.example.militaryservicecompanychecker.company.converter.GovernmentLocationConverter
 import com.example.militaryservicecompanychecker.company.converter.SectorConverter
 import com.example.militaryservicecompanychecker.company.enums.GovernmentLocation
@@ -10,41 +11,37 @@ import javax.persistence.*
 @Entity
 @Table(name = "company", schema = "PUBLIC", indexes = [Index(columnList = "name")])
 class Company(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
-
     @Column(name = "name")
-    val companyName: String,
+    var companyName: String,
 
     @Column(name = "government_location")
     @Convert(converter = GovernmentLocationConverter::class)
-    val governmentLocation: GovernmentLocation,
+    var governmentLocation: GovernmentLocation,
 
     @Column(name = "location")
-    val companyLocation: String,
+    var companyLocation: String,
 
     @Column(name = "phone_number")
     @JsonIgnore
-    val companyPhoneNumber: String,
+    var companyPhoneNumber: String,
 
     @Column(name = "fax_number")
     @JsonIgnore
-    val companyFaxNumber: String,
+    var companyFaxNumber: String,
 
     @Column(name = "sector")
     @Convert(converter = SectorConverter::class)
-    val companySector: Sector,
+    var companySector: Sector,
 
     @Column(name = "scale")
-    val companyScale: String,
+    var companyScale: String,
 
     @Column(name = "service_type")
-    val serviceType: String,
+    var serviceType: String,
 
     @Column(name = "keyword")
-    val companyKeyword: String,
+    var companyKeyword: String,
 
     @Column(name = "kreditjob_key")
-    var kreditJobKey: String,
-)
+    var kreditJobKey: String?,
+) : BaseEntity()
