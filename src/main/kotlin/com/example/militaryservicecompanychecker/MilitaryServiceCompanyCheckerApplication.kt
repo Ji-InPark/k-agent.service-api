49a65a9f4b0aab3a22dfaac04280fc5f4e4d4493
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-07-06 01:24:37 +0900

    Add EnableJpaAuditing annotation

--- src/main/kotlin/com/example/militaryservicecompanychecker/MilitaryServiceCompanyCheckerApplication.kt
+++ src/main/kotlin/com/example/militaryservicecompanychecker/MilitaryServiceCompanyCheckerApplication.kt
@@ -2,8 +2,10 @@ package com.example.militaryservicecompanychecker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class MilitaryServiceCompanyCheckerApplication
 
 fun main(args: Array<String>) {
